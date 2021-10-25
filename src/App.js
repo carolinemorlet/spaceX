c5b97aee47b3d60e884834d7e94c8b7c3706e532
Caroline Morlet <carolinemorlet@gmail.com>
2021-10-25 22:57:35 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useQuery, gql } from '@apollo/client';
+
+const GET_LAUNCHES = gql`
+  query GetLaunches {
+    launches(limit: 5) {
+      launch_date_utc
+      launch_success
+      rocket {
+        rocket_name
+      }
+      links {
+        video_link
+      }
+      details
+    }
+  }
+`;
 
 function App() {
+  const { loading, error, data } = useQuery(GET_LAUNCHES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : (</p>;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>my first apollo app</h1>
+      <hr />
+      {data.launches.map((launche, { launch_success, rocket, links }) => (
+        <ul>
+          <li>Launch date : {launche.launch_date_utc}</li>
+          <li>Success Launch : {launche.launch_success}</li>
+          <br />
+          <li>Rocket Name : {launche.rocket.rocket_name}</li>
+          <br />
+          <li>Link : {launche.links.video_link}</li>
+          <br />
+          <li>Description : {launche.details}</li>
+          <br />
+          <hr />
+        </ul>
+      ))}
     </div>
   );
 }
